0e94757a670d9f88f1fe603f85b16cd186ee90e0
Shreyas Soni <46019734+sonishreyas@users.noreply.github.com>
2021-09-20 21:57:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+var readlineSync = require('readline-sync');
+var score = 0;
+
+var highscore = [
+  {
+    name:"Sanket",
+    score: 3
+  },
+  {
+    name: "Mohit",
+    score: 2
+  }
+];
+
+var questions = [
+  {
+    question: "What is my Favourite Food? ",
+    answer: "Chicken"
+  },
+  {
+    question: "My Favourite IPL Team?",
+    answer: "CSK"
+  },
+  {
+    question: "Where do I live ?",
+    answer: "Pune"
+  } 
+];
+
+function welcome(){
+  var username = readlineSync.question("What's your name? ");
+  console.log("Welcome "+ username + " let's see if you know Shreyas");
+}
+
+function play(question,answer)  {
+  var userAnswer = readlineSync.question(question);
+
+  if(userAnswer.toUpperCase() === answer.toUpperCase()) {
+    console.log("right!");
+    score += 1;
+  } else  {
+    console.log("wrong!");
+  }
+  console.log("Current Score: ", score);
+  console.log("************");
+}
+
+function game() {
+  for (var i=0; i< questions.length;i++)  {
+    var currQues = questions[i];
+    play(currQues.question,currQues.answer);
+  }
+}
+
+function showScores() {
+  console.log("YAY! You Scored: ", score);
+  console.log("Check out the high scores");
+
+  highscore.map(score => console.log(score.name, " : ", score.score))
+}
+
+welcome();
+game();
+showScores();
